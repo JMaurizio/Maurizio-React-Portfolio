ec3559f9788458d464c367d8e380e5b2656d3672
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-02-26 18:48:54 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Nav from './components/Nav'
 import './App.css';
+import './assets/style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <main>
+        <section class="container-fluid d-flex flex-row align-items-center">
+            <div class="col-1">
+                <Nav></Nav>
+            </div>
+            <div class="col-10 offset-1 d-flex flex-wrap flex-column justify-content-center align-items-center">
+                <p class="h1 text-center offset-2" id="intro-p">
+                    Hello, I'm <span id="JMText">Joshua Maurizio</span> 
+                    and I'm a full-stack web developer. 
+                </p>
+            </div>    
+        </section>
+    </main>
   );
 }
 
