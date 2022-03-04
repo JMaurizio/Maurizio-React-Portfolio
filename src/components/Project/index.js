6078ef3c98c512d1310656e125601b548a0a7338
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-03-04 10:18:10 -0600

    added other project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import programmerDashboard from "../../assets/Images/programmer-dashboard.png"
+import chattleshipScreenshot from "../../assets/Images/chattleship-board-crop.png"
+import comingSoonImage from "../../assets/Images/smoky moutain placeholder.jpg"
 
 export default function Projects() {
 
     return (
         <div class="col-10 offset-1 d-flex flex-row flex-wrap justify-content-center align-items-center">
-            {/* <Card class="m-2" id="project-card">    
-                <Route path='/programmer-dashboard' component={() => {window.location.href= 'https://joelkovalcson.github.io/Programmer-Dashboard/'; return null}} class="text-decoration-none">
-                    <Card.Img  src={programmerDashboard} alt="programmer-dashboard screenshot"/>    
-                    <Card.ImgOverlay class="card-img d-flex align-items-center justify-content-center" id="project-image">
-                        <Card.Title class="card-title" id="project-title">Programmer Dashboard</Card.Title>
-                    </Card.ImgOverlay>
-                </Route>
-            </Card> */}
-            <Card onClick={() => {window.open('https://joelkovalcson.github.io/Programmer-Dashboard/','_blank');}} className="bg-dark text-white" id="project-card">
+            <Card onClick={() => {window.open('https://joelkovalcson.github.io/Programmer-Dashboard/','_blank');}} className="bg-dark text-white m-2" id="project-card">
                 <Card.Img src={programmerDashboard} id="project-image" alt="Programer Dashboard Screenshot" />
                 <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
                     <Card.Title id="project-title">Programmer Dashboard</Card.Title>
+                </Card.ImgOverlay>
+            </Card>
+            <Card onClick={() => {window.open('https://chattleship.herokuapp.com/','_blank');}} className="bg-dark text-white m-2" id="project-card">
+                <Card.Img src={chattleshipScreenshot} id="project-image" alt="Chattleship Screenshot" />
+                <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
+                    <Card.Title id="project-title">Chattleship</Card.Title>
+                </Card.ImgOverlay>
+            </Card>
+            <Card onClick={() => {window.open('https://chattleship.herokuapp.com/','_blank');}} className="bg-dark text-white m-2" id="project-card">
+                <Card.Img src={comingSoonImage} id="project-image" alt="Moutain placeholder image" />
+                <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
+                    <Card.Title id="project-title">Coming Soon</Card.Title>
+                </Card.ImgOverlay>
+            </Card>
+            <Card onClick={() => {window.open('https://chattleship.herokuapp.com/','_blank');}} className="bg-dark text-white m-2" id="project-card">
+                <Card.Img src={comingSoonImage} id="project-image" alt="Moutain placeholder image" />
+                <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
+                    <Card.Title id="project-title">Coming Soon</Card.Title>
+                </Card.ImgOverlay>
+            </Card>
+            <Card onClick={() => {window.open('https://chattleship.herokuapp.com/','_blank');}} className="bg-dark text-white m-2" id="project-card">
+                <Card.Img src={comingSoonImage} id="project-image" alt="Moutain placeholder image" />
+                <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
+                    <Card.Title id="project-title">Coming Soon</Card.Title>
                 </Card.ImgOverlay>
             </Card>
         </div>    
