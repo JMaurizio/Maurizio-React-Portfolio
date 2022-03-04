4794ce924dd635efbdfeba8e0ffdd012bbf15af5
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-03-04 14:24:50 -0600

    connected form to email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,44 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser'
+import { Form, Button } from "react-bootstrap";
 
 export default function Contact() {
+
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            "service_gmvq74u",
+            "template_90a9mqm",
+            form.current,
+            "vTYsIeRHKADboDw9W"
+        )
+        .then((result) => {
+            console.log(result.text);
+        },
+        (error) => {
+            console.log(error.text)
+        });
+    };
     
     return (
         <div class="col-10 offset-1 d-flex flex-row flex-wrap justify-content-center align-items-center">
-            <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form ref={form} onSubmit={sendEmail}>
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" placeholder="Name" name="user_name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="email">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control type="email" placeholder="name@example.com" name="user_email" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="textarea">
                     <Form.Label>Your message here</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={6} name="message" />
                 </Form.Group>
+                <Button variant="primary" type="submit" value="Send">Submit</Button>
             </Form>
         </div>    
     )
