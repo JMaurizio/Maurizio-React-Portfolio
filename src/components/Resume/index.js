66235b8c8a031e05ee8cbdb103218cabb21c8e2d
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-03-04 18:19:41 -0600

    update resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import JMResume from "../../assets/Images/Joshua Maurizio Resume1024_1.jpg"
-import { Image } from "react-bootstrap";
 
 export default function Resume() {
     return (
         <div class="col-8 offset-1 d-flex flex-column justify-content-center align-items-center">
-            <Image className="fluid" src={JMResume} alt="Joshua Maurizio Resume"></Image>
+            <iframe title="JMResume" src={JMResume} width="100%" height="550" alt="Joshua Maurizio Resume"></iframe>
         </div>    
     )
 }
